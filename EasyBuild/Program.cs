9148f072d6e41a51dd1b9d4fe75cbbc49910486a
LuraMoth <85266594+Luramoth@users.noreply.github.com>
2024-06-01 01:15:04 -0700

    Add project files.

---
+++ EasyBuild/Program.cs
@@ -0,0 +1,48 @@
+﻿using EasyBuild;
+using System.IO;
+
+static class Program
+{
+    enum ArgMode
+    {
+        None,
+		ProjPath
+    }
+
+	static ArgMode argMode = ArgMode.None;
+
+    static void Main(string[] args)
+	{
+		Project project = new Project();
+
+		if (args[0].ToLower() == "build")
+		{
+			foreach (string arg in args)
+			{
+				switch (arg.ToLower()) {
+					case "debug":
+						project.BuildType = BuildType.Debug;
+						continue;
+					case "release":
+						project.BuildType = BuildType.Release;
+						continue;
+					case "-p":
+						argMode = ArgMode.ProjPath;
+
+						continue;
+					default:
+						switch (argMode)
+						{
+							case ArgMode.ProjPath:
+								project.ProjectDir = arg;
+								argMode = ArgMode.None;
+								continue;
+						}
+						continue;
+				}
+			}
+
+			project.Build();
+		}
+	}
+}
---
+++ EasyBuild/Project.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+
+namespace EasyBuild
+{
+	enum BuildType
+	{
+		Debug,
+		Release
+	}
+
+	internal class Project
+	{
+		public string? ProjectDir;
+
+		public BuildType BuildType;
+
+		private string? BuildFile;
+
+		public int Build()
+		{
+			string[] files;
+
+			try
+			{
+				ProjectDir = Path.GetFullPath(ProjectDir!);
+
+				Directory.SetCurrentDirectory(ProjectDir!);
+
+				files = Directory.GetFiles(ProjectDir!);
+			}
+			catch
+			{
+				Console.WriteLine("PreBuild failed, invalid project directory: " + Path.GetFullPath(ProjectDir!));
+				return 1;
+			}
+
+			foreach (string file in files)
+			{
+				if (file.ToLower() == "build.easybuild")
+				{
+					BuildFile = file;
+				}
+			}
+
+			if (BuildFile is null)
+			{
+				Console.WriteLine("PreBuild failed, build.easybuild not found in project directory: " + ProjectDir!);
+				return 1;
+			}
+
+			return 0;
+		}
+	}
+}
