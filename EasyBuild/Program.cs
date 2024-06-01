859e4be08d346a151f9bcbadea39f18c94324d3d
LuraMoth <85266594+Luramoth@users.noreply.github.com>
2024-06-01 03:06:14 -0700

    add version print

--- EasyBuild/Program.cs
+++ EasyBuild/Program.cs
@@ -7,31 +7,43 @@ using System.IO;
 
 static class Program
 {
-    enum ArgMode
-    {
-        None,
+	enum ArgMode
+	{
+		None,
 		ProjPath
-    }
+	}
 
 	static ArgMode argMode = ArgMode.None;
 	static readonly Project project = new();
 
 	static void Main(string[] args)
 	{
+		if (args.Length == 0)
+		{
+			Console.WriteLine("Error, no arguments given, use -h for help");
+			Console.WriteLine("Usage: easybuild <build|test> [debug|release] [-p <path>] \n");
+
+			return;
+		}
+
 		if (args[0].Equals("build", StringComparison.CurrentCultureIgnoreCase))
 		{
 			HandleArgs(args.Skip(1).ToArray());
 
 			project.Build();
 		}
-        else if (args[0].Equals("test", StringComparison.CurrentCultureIgnoreCase))
+		else if (args[0].Equals("test", StringComparison.CurrentCultureIgnoreCase))
 		{
 			HandleArgs(args.Skip(1).ToArray());
 
 			project.Build();
 			project.Test();
 		}
-    }
+		else if (args[0].Equals("-v", StringComparison.CurrentCultureIgnoreCase))
+		{
+			PrintVersion();
+		}
+	}
 
 	static private void HandleArgs(string[] args)
 	{
@@ -62,5 +74,13 @@ static class Program
 					continue;
 			}
 		}
+	}
+
+	static private void PrintVersion()
+	{
+		Console.WriteLine("\nEasyBuild version Alpha6.24");
+		Console.WriteLine("A program to make building your projects easy\n");
+		Console.WriteLine("This program is Liscenced under the Mozilla Public License, v. 2.0");
+		Console.WriteLine("If the MPL was not distributed with your copy of EasyBuild, you can obtain one at http://mozilla.org/MPL/2.0/.\n");
 	}
 }
